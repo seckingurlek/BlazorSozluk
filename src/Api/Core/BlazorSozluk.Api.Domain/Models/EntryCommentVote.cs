5f1ca75575b112ef8937a86a37e5992e69799b09
seckingurlek <seckingurlek45@gmail.com>
2025-02-05 14:56:23 +0300

    api entities mediatr

---
+++ src/Api/Core/BlazorSozluk.Api.Domain/Models/EntryCommentVote.cs
@@ -0,0 +1,17 @@
+﻿using BlazorSozluk.Common.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorSozluk.Api.Domain.Models
+{
+    public class EntryCommentVote: BaseEntity
+    {
+        public VoteType voteType { get; set; }
+        public Guid EntryCommentId { get; set; }
+        public Guid CreatedById { get; set; }
+        public virtual EntryComment EntryComment { get; set; }
+    }
+}
---
+++ src/Api/WebApi/BlazorSozluk.Api.WebApi/Program.cs
@@ -0,0 +1,37 @@
+
+namespace BlazorSozluk.Api.WebApi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+            //uygulama build edildikten sonra çalýþna kýsým
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
